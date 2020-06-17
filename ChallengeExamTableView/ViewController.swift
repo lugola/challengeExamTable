d7bdf129c5d31bb9523f540d3b0770d4472611f4
Moises Lugo <moiseslaguilar@gmail.com>
2020-06-16 23:48:13 -0500

    Initial Commit

---
+++ ChallengeExamTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChallengeExamTableView
+//
+//  Created by Moises Lugo on 16/06/20.
+//  Copyright © 2020 challengeTableView. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
